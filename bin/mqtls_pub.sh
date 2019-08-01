325d570f1d84340c8b32596ea4c83ef9af17de62
Hyunwoo <hw5773@gmail.com>
2019-08-01 17:07:19 +0900

    add libraries

--- bin/mqtls_pub.sh
+++ bin/mqtls_pub.sh
@@ -1,3 +1,3 @@
 PWD=`pwd`
-export LD_LIBRARY_PATH=$PWD/../tlsps-lib:$PWD/../openssl/lib
+export LD_LIBRARY_PATH=$PWD/../tlsps-lib:$PWD/../openssl-nodebug/lib
 ./mosquitto_tlsps_pub -t hello -m test --cafile ../certs/demoCA/cacert.pem -h www.rsa.com --key ../certs/publisher_priv.pem --cert ../certs/publisher_pub.pem
